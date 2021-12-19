9f066eee3f368f5b9fd87be3508b88da7927310d
kateb <katebulate@gmail.com>
2021-12-19 17:31:02 +0300

    fixed wav generation

--- test_wav.py
+++ test_wav.py
@@ -5,7 +5,7 @@ import librosa
 import config
 from model.generator import Generator
 from utils.utils import log_audio, load_waveform
-from dataloader.meldataset import mel_spectrogram
+from dataloader.melspec import get_featurizer
 
 
 if torch.cuda.is_available():
@@ -23,11 +23,12 @@ if __name__ == '__main__':
 
     model.eval()
     model.remove_weight_norm()
+    featurizer = get_featurizer()
     with torch.no_grad():
         for i, filename in enumerate(files):
             y, s = librosa.load(os.path.join(config.test_wavs_dir, filename), sr=config.sr)
 
             wav = torch.Tensor(y).unsqueeze(0)
-            x = mel_spectrogram(wav)
+            x = featurizer(wav)
             predicted = model(x)
             log_audio(predicted, filename[:-4] + '_gen.wav')
